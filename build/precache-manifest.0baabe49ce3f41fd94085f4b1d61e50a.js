75c36150dd6813be0ee84e9067100aa8485f8e45
nteguem roland <nteguemroland@gmail.com>
2021-02-08 20:10:10 +0100

    initialize builddd

---
+++ build/precache-manifest.0baabe49ce3f41fd94085f4b1d61e50a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da25fc8fa12adfb9e011ee3bac14b9af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312a4f4b15bbd2f031e0",
+    "url": "/static/css/main.1b60b8fe.chunk.css"
+  },
+  {
+    "revision": "7ca0c2c506f94173009e",
+    "url": "/static/js/2.adc0c3a4.chunk.js"
+  },
+  {
+    "revision": "7221a43b3dc3df0f78c0595f3997f5e1",
+    "url": "/static/js/2.adc0c3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312a4f4b15bbd2f031e0",
+    "url": "/static/js/main.44aab5b4.chunk.js"
+  },
+  {
+    "revision": "791180c0378bc1c75391aee648563762",
+    "url": "/static/js/main.44aab5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3644564eab1d61d8b262",
+    "url": "/static/js/runtime-main.ae429f58.js"
+  },
+  {
+    "revision": "8816078f9fed8bb01cf91db14079ebc7",
+    "url": "/static/media/backgroundLogin.8816078f.png"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "fd79d99840cd0340c995e706fb75c7a9",
+    "url": "/static/media/loader.fd79d998.gif"
+  },
+  {
+    "revision": "3216f1ce9d2f28cfa84026da1be035e6",
+    "url": "/static/media/notification_big_win.3216f1ce.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "45899c888e0ad26b3ebe60efcc4d4904",
+    "url": "/static/media/pp.45899c88.jpg"
+  },
+  {
+    "revision": "7ae9e98de50d629d160972977d5b8091",
+    "url": "/static/media/react-logo.7ae9e98d.png"
+  },
+  {
+    "revision": "b059054d0b7e6fbf8941f8520e0c80cd",
+    "url": "/static/media/soccer.b059054d.jpg"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  }
+]);
